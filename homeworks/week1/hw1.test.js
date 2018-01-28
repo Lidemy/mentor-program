16d306f95db37867f88bc851f9afb2c3d162bec4
huli <aszx87410@gmail.com>
2018-01-28 10:42:03 +0800

    fix test case

--- homeworks/week1/hw1.test.js
+++ homeworks/week1/hw1.test.js
@@ -2,6 +2,6 @@ import {stars} from './hw1'
 
 describe("hw1", () => {
   it("should return correct answer when n = 1", () => {
-    expect(stars(1)).toBe('*')
+    expect(stars(1)).toEqual(['*'])
   })
 })
